2c31963878d9e0a18431c8bead1901fcbeb18db4
unknown <ricardo.queiros@gmail.com>
2018-07-30 10:54:58 +0100

    First commit

---
+++ js/webconf.js
@@ -0,0 +1,47 @@
+window.onload = function() {
+  const btnRegister = document.getElementById("btnRegister");
+
+  btnRegister.addEventListener("click", function() {
+    swal({
+      title: "Inscrição na WebConference",
+      input: "email",
+      inputPlaceholder: "email",
+      inputAttributes: {
+        autocapitalize: "off"
+      },
+      showCancelButton: true,
+      confirmButtonText: "Inscrever",
+      cancelButtonText: "Cancelar",
+      showLoaderOnConfirm: true,
+      preConfirm: email => {
+        return fetch("https://fcawebbook.herokuapp.com/participants", {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          method: "POST",
+          body: JSON.stringify({
+            nome: "ricardo",
+            email: "ricardo.queiros@gmail.com"
+          })
+        })
+          .then(response => {
+            if (!response.ok) {
+              throw new Error(response.statusText);
+            }
+            return response.json();
+          })
+          .catch(error => {
+            swal.showValidationError(`Request failed: ${error}`);
+          });
+      },
+      allowOutsideClick: () => !swal.isLoading()
+    }).then(result => {
+      if (result.value) {
+        swal({
+          title: `${result}`
+          //imageUrl: result.value.avatar_url
+        });
+      }
+    });
+  });
+};
